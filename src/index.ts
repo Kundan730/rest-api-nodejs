0f09d136ecaa5f66e0b01e79b76bf238494b9dfb
Kundan Kumar Sahu <espkundan@gmail.com>
2023-07-01 13:53:10 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+require('dotenv').config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = 8080;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+const MONGO_URL = process.env.DATABASE_URL
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL)
+.then(() => {
+  console.log('Connected to MongoDB');
+  // Start your application logic here
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+mongoose.connection.on('error', (error: Error) => console.log(error));
