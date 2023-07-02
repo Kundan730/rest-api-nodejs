b012c7685be09510ca9b9fd610ec6f4a56945c61
Kundan Kumar Sahu <espkundan@gmail.com>
2023-07-02 20:21:21 +0530

    prettier pre-commit husky test

--- src/index.ts
+++ src/index.ts
@@ -31,34 +31,22 @@ const port = 8080;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-
-
-
   console.log(`Server is running on http://localhost:${port}`);
-
-
 });
 
 const MONGO_URL = process.env.DATABASE_URL;
 
 mongoose.Promise = Promise;
 
-
 mongoose
   .connect(MONGO_URL)
 
   .then(() => {
-
-
     console.log("Connected to MongoDB");
   })
 
-  .
-  catch((error) => {
-
+  .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
-
-
   });
 
 mongoose.connection.on("error", (error: Error) => console.log(error));
